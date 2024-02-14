367168487a9ed08053ef6b4e90f19540bd3260f6
CRSampaio <c.r.sampaio@outlook.com>
2024-02-14 02:14:26 -0300

    refact(injeção dependência): referenciado apenas projeto da aplicação na API

--- src/FinActions.Application/DependencyInjection.cs
+++ src/FinActions.Application/DependencyInjection.cs
@@ -1,11 +1,15 @@
+using FinActions.Domain.AppSettings;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FinActions.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services,
+        ConnectionStrings connectionStrings)
     {
+        services.AddInfrastructureServices(connectionStrings);
         services.AddTransient<Contracts.Usuario.IUsuarioService, Application.Usuarios.UsuarioService>();
         services.AddTransient<Application.Token.ITokenService, Application.Token.TokenService>();
 
