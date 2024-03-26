1e2adbd4d2ede7e6d86d8129b10931deff7163eb
CRSampaio <c.r.sampaio@outlook.com>
2024-03-25 22:35:04 -0300

    typo(token): corrigido formatação

--- src/FinActions.Domain/AppSettings/JwtOptions.cs
+++ src/FinActions.Domain/AppSettings/JwtOptions.cs
@@ -2,8 +2,8 @@ namespace FinActions.Domain.AppSettings;
 
 public class JwtOptions
 {
-    public string Issuer {get; set;} = null!;
-    public string Audience {get; set;} = null!;
-    public string SecurityKey {get; set;} = null!;
-    public int ExpirationInSeconds {get; set;}
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string SecurityKey { get; set; } = null!;
+    public int ExpirationInSeconds { get; set; }
 }
